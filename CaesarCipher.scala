7a87a13abe3c21dbbf7e4cc08a1f6518582ae87c
VinoVK08 <62206610+VinoVK08@users.noreply.github.com>
2021-08-09 08:34:44 +0100

    Add files via upload

---
+++ CaesarCipher.scala
@@ -0,0 +1,37 @@
+//19001762
+import scala.io.StdIn._
+
+object CaesarCipher extends App{
+def cipher(t:String,s:Int):String= {
+  var result = "";
+  var i = 0;
+  for(i <-0 until t.length()){
+    if((t.charAt(i).isUpper)){
+      var ch = ((t.charAt(i).toInt + s - 65) % 26 + 65).toChar;
+      var str = ch.toString;
+      result = result.concat(str);
+    }else{
+      var ch = ((t.charAt(i).toInt + s - 97) % 26 + 97).toChar;
+      var str = ch.toString;
+      result = result.concat(str);
+    }
+  }
+  return result;
+}
+
+println("Select the number of operation : ")
+println("1. encrypt: ")
+println("2. decrypt: ")
+var option = scala.io.StdIn.readInt()
+
+val t = scala.io.StdIn.readLine("Enter the text: ")
+println("Enter the number of shifts: ")
+var s = scala.io.StdIn.readInt()
+if (option==1){
+  println(cipher(t,s))
+}else if(option==2){
+  println(cipher(t,26-s))
+}else{
+  println("invalid option")
+}
+}
